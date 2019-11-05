8bf20abebd74dd28affe244f334b2b19eaaeb9aa
Picroc <kingofrootelement@gmail.com>
2019-11-06 01:31:25 +0300

    API is WORKING

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import Login from './templates/login-form';
 import './assets/fonts.css';
 
+telegramApi.setConfig({
+    app: {
+        id: 1166576, /* App ID */
+        hash: '99db6db0082e27973ee4357e4637aadc', /* App hash */
+        version: '0.0.1' /* App version */
+    },
+    server: {
+        test: [
+            {
+                id: 2, /* DC ID */
+                host: '149.154.167.40',
+                port: 443
+            }
+        ],
+        production: [
+            {
+                id: 2, /* DC ID */
+                host: '149.154.167.50',
+                port: 443
+            }
+        ]
+    }
+});
+
+telegramApi.getUserInfo().then(res => { console.log("User info:", res); });
+telegramApi.sendCode('+79821759743');
+
 const q = (elem) => document.querySelector(elem);
 const App = q('.root');
 
