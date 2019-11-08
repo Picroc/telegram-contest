53c8609279cacfb270a4c4d7a421441ff4048ff5
Picroc <kingofrootelement@gmail.com>
2019-11-08 06:30:59 +0300

    Removed misc

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
 const merge = require('webpack-merge');
 
 
